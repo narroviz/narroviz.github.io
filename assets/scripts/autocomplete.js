778b3a84400ee7424891efee50ed2d0ec43f40af
dmnewcomb <dnewcomb23@gmail.com>
2020-06-29 15:42:29 -0700

    Adding data/scripts/image assets for around the world article

---
+++ assets/scripts/autocomplete.js
@@ -0,0 +1,22 @@
+var autocomplete;
+
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+    {types: []});
+
+  // When the user selects an address from the dropdown, populate the address
+  // fields in the form.
+  autocomplete.addListener('place_changed', fillInAddress);
+}
+
+function fillInAddress() {
+  // Get the place details from the autocomplete object.
+  var place = autocomplete.getPlace();
+  const latitude = place.geometry.location.lat()
+  const longitude = place.geometry.location.lng()
+  document.getElementById('latitude').setAttribute("value", latitude);
+  document.getElementById('longitude').setAttribute("value", longitude);
+}
